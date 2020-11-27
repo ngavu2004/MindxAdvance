3f23737677c4517f36a3111dbd0581fac9020d71
ngavu2004 <42512473+ngavu2004@users.noreply.github.com>
2020-11-27 18:59:36 +0700

    2

--- bai1.js
+++ bai1.js
@@ -1,16 +1,1 @@
-let body1 = document.getElementById("list");
-
-function createToDoList(todo) {
-    let newli = document.createElement("li");
-    newli.innerHTML = todo;
-    body1.appendChild(newli);
-}
-
-todos = ["Lap trinh", "An", "Ngu", "Choi", "Tam"]
-todos.forEach(createToDoList)
-// createToDoList("Lap trinh");
-// createToDoList("An");
-// createToDoList("Ngu");
-// createToDoList("Choi");
-// createToDoList("Tam");
 
