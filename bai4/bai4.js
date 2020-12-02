f5a2570e60bb13531bb66dcedec7d94465aba364
ngavu2004 <42512473+ngavu2004@users.noreply.github.com>
2020-12-02 20:26:09 +0700

    sqrt + ^2

--- bai4/bai4.js
+++ bai4/bai4.js
@@ -3,11 +3,12 @@ let num = 0
 let ternary=""
 function hienso(input) {
     let a = document.getElementById("screen").value
-    if (input=="+" || input=="*" || input=="/" || input=="-") {
+    if (input=="+" || input=="*" || input=="/" || input=="-" || input=="sqrt()" || input=="x^2") {
         rel = Number(a)
         ternary = input
         console.log(rel);
-        document.getElementById("screen").value = ""}
+        document.getElementById("screen").value = ""
+    }
     else if (input == "Clear") {
         rel = 0
         num = 0
@@ -33,7 +34,16 @@ function hienso(input) {
             rel /= num
             document.getElementById("screen").value = rel
         }
-        
+        if (ternary == "sqrt()") {
+            rel = Math.sqrt(rel)
+            document.getElementById("screen").value = rel
+        }
+
+        if (ternary == "x^2") {
+            rel = rel*rel
+            document.getElementById("screen").value = rel
+        }
+        console.log(rel);
     } else if (Number(input) != NaN) {
          document.getElementById("screen").value = a + input.toString();
     } 
